c396e94c6255b3abbd5d110e83bcfd9ba7dce8d4
Antarip Giri <antarip7993@gmail.com>
2024-09-21 20:33:55 +0530

    Adding changes to the main app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField # Field and Submit button for the forms
+from wtforms.validators import DataRequired # Validation check for the string field
 
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret_key_for_flasker" # csrf token
+
+# Create a Form Class
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
 
 # Create a route decorator
 
@@ -17,7 +29,6 @@ def index():
     first_name=first_name,
     test=test_sentence,
     favorite_pizza=favorite_pizza)
-
 
 # User Name Route
 @app.route("/user/<name>")
@@ -35,3 +46,14 @@ def page_not_found(error):
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template("500.html"), 500 # This is syntax
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
